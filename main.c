a27f9fb1c0c00c675c633058f191e086751afb56
kayer <Ronald.Kayejr@cognizant.com>
2009-08-27 11:01:01 -0400

    bug in original master

--- main.c
+++ main.c
@@ -7,5 +7,9 @@ int main(int argc, char **argv)
     printf ("Hello world, addition 1\n");
     printf ("For debug only\n");
 
+    printf ("Add your lastname\n");
+
+    printf ("Bug from original master\n");
+
     return 0;
 }
